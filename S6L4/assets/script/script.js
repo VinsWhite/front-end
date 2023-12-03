a972e1e1d1e802bb3ea70c82ace7e88a704b1cdb
Vincenzo <vincenzosa26@icloud.com>
2023-12-03 18:01:24 +0100

    Esercizio 20

---
+++ S6L4/assets/script/script.js
@@ -0,0 +1,8 @@
+/* DROPDOWN */
+
+/* const dropToggle = document.querySelector('.dropdown-toggle');
+const dropItem = document.querySelector('.dropdown-item');
+
+dropToggle.addEventListener('click', () => {
+    dropItem.style.display = dropItem.style.display === 'none' ? 'block' : 'none';
+}); */
